ce23b16eec619b783a7a1af81a340204109e92d7
amorist <amor90s.zhang@gmail.com>
2019-09-06 17:28:30 +0800

    add:Database CollectionNames()

--- database.go
+++ database.go
@@ -1,18 +1,29 @@
 package mango
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 // Database mongo-driver database
 type Database struct {
 	database *mongo.Database
 }
 
-// C returns collection
+// CollectionNames returns the collection names present in database.
+func (d *Database) CollectionNames() (names []string, err error) {
+	names, err = d.database.ListCollectionNames(context.TODO(), options.ListCollectionsOptions{})
+	return
+}
+
+// C returns collection.
 func (d *Database) C(collection string) *Collection {
 	return &Collection{collection: d.database.Collection(collection)}
 }
 
-// Collection returns collection
+// Collection returns collection.
 func (d *Database) Collection(collection string) *Collection {
 	return &Collection{collection: d.database.Collection(collection)}
 }
